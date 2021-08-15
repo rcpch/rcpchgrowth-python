725f1488abb2c452be9735811d15096be28731ab
Simon Chapman <eatyourpeasapps@gmail.com>
2021-08-15 19:26:01 +0100

    add comments to measurement class, bone age class, refactor

--- rcpchgrowth/bone_age.py
+++ rcpchgrowth/bone_age.py
@@ -13,14 +13,16 @@ class BoneAge:
         self,
         decimal_age: float,
         bone_age: float,
+        height: float,
         bone_age_type: str = None,
         bone_age_sds: float = None,
         bone_age_centile: float = None,
-        bone_age_text: str = None
+        bone_age_text: str = None,
         ):
 
         self.decimal_age = decimal_age
         self.bone_age = bone_age
+        self.height=height
         self.bone_age_type = bone_age_type
         self.bone_age_sds = bone_age_sds
         self.bone_age_centile = bone_age_centile
@@ -32,5 +34,15 @@ class BoneAge:
             "bone_age_type": self.bone_age_type,
             "bone_age_sds": self.bone_age_sds,
             "bone_age_centile": self.bone_age_centile,
-            "bone_age_text": self.bone_age_texte
+            "bone_age_text": self.bone_age_text,
+            "plottable_data": [
+                {
+                    "x": self.decimal_age,
+                    "y": self.height
+                },
+                {
+                    "x": self.decimal_age,
+                    "y": self.bone_age
+                }
+            ]
         }
