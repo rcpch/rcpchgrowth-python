3e96c439ea32932c46f65fde5aef5b6ea2115963
Simon Chapman <eatyourpeasapps@gmail.com>
2022-02-16 19:37:39 +0000

    remove suffix function from __init__.py

--- rcpchgrowth/__init__.py
+++ rcpchgrowth/__init__.py
@@ -1,6 +1,6 @@
 from .date_calculations import chronological_decimal_age, corrected_decimal_age, chronological_calendar_age, estimated_date_delivery, corrected_gestational_age
 from .global_functions import centile, sds_for_measurement, measurement_from_sds, percentage_median_bmi, mid_parental_height, measurement_for_z
-from .centile_bands import centile_band_for_centile, return_suffix
+from .centile_bands import centile_band_for_centile
 from .bmi_functions import bmi_from_height_weight, weight_for_bmi_height
 from .age_advice_strings import comment_prematurity_correction
 from .fictional_child import generate_fictional_child_data
