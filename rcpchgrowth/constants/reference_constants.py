b4c6aed65d716188ebba7ef8a9cc4c6ac119f7a5
eatyourpeas <simon@eatyourpeas.co.uk>
2024-09-16 22:15:48 +0100

    add the cdc data (minus ofc), add threshold constants, start cdc.json

--- rcpchgrowth/constants/reference_constants.py
+++ rcpchgrowth/constants/reference_constants.py
@@ -18,7 +18,8 @@ UK_WHO_REFERENCES = [UK90_PRETERM, UK_WHO_INFANT, UK_WHO_CHILD, UK90_CHILD]
 
 # 23 weeks is the lowest decimal age available on the UK90 charts
 UK90_REFERENCE_LOWER_THRESHOLD = (
-    (23 * 7) - (40 * 7)) / 365.25  # 23 weeks as decimal age
+    (23 * 7) - (40 * 7)
+) / 365.25  # 23 weeks as decimal age
 
 # The WHO references change from measuring infants in the lying position to measuring children in the standing position at 2.0 years.
 WHO_CHILD_LOWER_THRESHOLD = 2.0  # 2 years as decimal age
@@ -33,31 +34,44 @@ WHO_CHILD_LOWER_THRESHOLD = 2.0  # 2 years as decimal age
 
 WHO_CHILDREN_UPPER_THRESHOLD = 4.0
 UK_WHO_INFANT_LOWER_THRESHOLD = (
-    (42 * 7) - (40 * 7)) / 365.25  # 42 weeks as decimal age
+    (42 * 7) - (40 * 7)
+) / 365.25  # 42 weeks as decimal age
 UK90_UPPER_THRESHOLD = 20
+
+WHO_NEWBORN_LOWER_THRESHOLD = 0.0
+CDC_LOWER_THRESHOLD = 2.0
+CDC_UPPER_THRESHOLD = 20.0
 
 
 # Generic constants
-MALE="male"
-FEMALE="female"
-HEIGHT="height"
-WEIGHT="weight"
-HEAD_CIRCUMFERENCE="ofc"
-BMI="bmi"
+MALE = "male"
+FEMALE = "female"
+HEIGHT = "height"
+WEIGHT = "weight"
+HEAD_CIRCUMFERENCE = "ofc"
+BMI = "bmi"
 REFERENCES = [UK_WHO, TRISOMY_21, TURNERS]
 SEXES = [MALE, FEMALE]
 MEASUREMENT_METHODS = [HEIGHT, WEIGHT, HEAD_CIRCUMFERENCE, BMI]
 
 # Nomenclature
 # Centile formats describe the collection - the two most common used are the cole-nine-centiles format used in UK-WHO
-# and the three-percent-centiles used by the CDC. They use 5/10/25/50/75/90/95 and suggest 3rd centile 
+# and the three-percent-centiles used by the CDC. They use 5/10/25/50/75/90/95 and suggest 3rd centile
 # is unlikely necessary.
 
-THREE_PERCENT_CENTILES = 'three-percent-centiles'
-COLE_TWO_THIRDS_SDS_NINE_CENTILES = 'cole-nine-centiles'
-CENTILE_FORMATS=[THREE_PERCENT_CENTILES, COLE_TWO_THIRDS_SDS_NINE_CENTILES]
+THREE_PERCENT_CENTILES = "three-percent-centiles"
+COLE_TWO_THIRDS_SDS_NINE_CENTILES = "cole-nine-centiles"
+CENTILE_FORMATS = [THREE_PERCENT_CENTILES, COLE_TWO_THIRDS_SDS_NINE_CENTILES]
 
-THREE_PERCENT_CENTILE_COLLECTION = [
-    3.0, 5.0, 10.0, 25.0, 50.0, 75.0, 90.0, 95.0, 97.0]
+THREE_PERCENT_CENTILE_COLLECTION = [3.0, 5.0, 10.0, 25.0, 50.0, 75.0, 90.0, 95.0, 97.0]
 COLE_TWO_THIRDS_SDS_NINE_CENTILE_COLLECTION = [
-    0.4, 2.0, 9.0, 25.0, 50.0, 75.0, 91.0, 98.0, 99.6]
+    0.4,
+    2.0,
+    9.0,
+    25.0,
+    50.0,
+    75.0,
+    91.0,
+    98.0,
+    99.6,
+]
