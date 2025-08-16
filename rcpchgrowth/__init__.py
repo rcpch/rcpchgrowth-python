bc0fe15a6040298df383eb2d8ece971067a40e12
eatyourpeas <simon@eatyourpeas.co.uk>
2025-08-16 08:24:31 +0100

    remove bump2version and _version.py

--- rcpchgrowth/__init__.py
+++ rcpchgrowth/__init__.py
@@ -14,4 +14,3 @@ from .trisomy_21 import select_reference_data_for_trisomy_21
 from .trisomy_21_aap import select_reference_data_for_trisomy_21_aap
 from .turner import select_reference_data_for_turner
 from .uk_who import select_reference_data_for_uk_who_chart
-from ._version import __version__  # single-source version
